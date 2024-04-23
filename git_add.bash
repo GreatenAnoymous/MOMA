f48b4cbcd6aedf0f64c2c12e2caed7f108db08d0
2274880117@qq.com <>
2024-04-23 16:53:57 -0400

    upload code

---
+++ git_add.bash
@@ -0,0 +1,5 @@
+git add ./metric_depth/zoedepth/data/*.py
+git add ./metric_depth/zoedepth/models/base_models/*.py
+git add ./metric_depth/zoedepth//models/base_models/dpt_dinov2/*.py
+git add ./metric_depth/zoedepth//trainers/*.py
+git add ./metric_depth/zoedepth//utils/*.py
